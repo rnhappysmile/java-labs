94c29e470a3fd18eeabeca9c8dc8662c34f42b8d
rnhappysmile <rnhappysmile@gmail.com>
2026-03-10 18:04:10 +0900

    feat: Initialize Spring Boot project

---
+++ src/main/java/com/rnhappysmile/java_labs/JavaLabsApplication.java
@@ -0,0 +1,13 @@
+package com.rnhappysmile.java_labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rnhappysmile/java_labs/JavaLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rnhappysmile.java_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
